a6c6f210a4a5cbba54711c6459bdb84c304544b1
pkruk <pkruk94@gmail.com>
2025-12-20 12:56:54 +0100

    Changed bucket name and added backend config

--- terraform/aws-backend/main.tf
+++ terraform/aws-backend/main.tf
@@ -1,4 +1,11 @@
 terraform {
+  # backend "s3" {
+  #   bucket         = "my-portfolio-infra-tf-state"
+  #   key            = "my-portfolio/terraform.tfstate"
+  #   region         = "us-east-1"
+  #   dynamodb_table = "terraform-state-locking"
+  #   encrypt        = true
+  # }
 
   required_providers {
     aws = {
--- terraform/aws-backend/variables.tf
+++ terraform/aws-backend/variables.tf
@@ -1,7 +1,7 @@
 variable "bucket_name" {
   description = "Name of the S3 bucket for static content storage."
   type        = string
-  default     = "my-portfolio-bucket-4815162342108"
+  default     = "my-portfolio-bucket"
 }
 
 variable "cf_cache_token" {
