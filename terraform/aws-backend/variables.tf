256ae42ecd939b34dda2daae2a7a6c1807d158b9
pkruk <pkruk94@gmail.com>
2025-12-20 12:19:20 +0100

    Added bootstrap configuration for AWS

--- terraform/aws-backend/variables.tf
+++ terraform/aws-backend/variables.tf
@@ -1,5 +1,23 @@
 variable "bucket_name" {
   description = "Name of the S3 bucket for static content storage."
-  type = string
-  default = "my-portfolio-bucket"
+  type        = string
+  default     = "my-portfolio-bucket-4815162342108"
+}
+
+variable "cf_cache_token" {
+  description = "Cloudflare API token for cache invalidation"
+  type        = string
+  sensitive   = true
+}
+
+variable "cf_dns_token" {
+  description = "CloudFlare API token for DNS setup"
+  type        = string
+  sensitive   = true
+}
+
+variable "cloudflare_zone_id" {
+  description = "Cloudflare Zone ID"
+  type        = string
+  sensitive   = true
 }
