1797b10dd86ce566e49ecc584e6d54a85e1c3d68
pkruk <pkruk94@gmail.com>
2025-12-21 15:35:35 +0100

    Add API GW config

---
+++ terraform/infra/apigw.tf
@@ -0,0 +1,38 @@
+resource "aws_apigatewayv2_api" "lambda_api" {
+  name          = "visitor-counter-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "lambda_stage" {
+  api_id = aws_apigatewayv2_api.lambda_api.id
+  name   = "$default"
+  auto_deploy = true
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id           = aws_apigatewayv2_api.lambda_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri = aws_lambda_function.visit_counter_lambda.invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "lambda_route" {
+  api_id = aws_apigatewayv2_api.lambda_api.id
+  route_key = "POST /"
+  target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_lambda_permission" "api_gw_invoke" {
+  statement_id = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.visit_counter_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "${aws_apigatewayv2_api.lambda_api.execution_arn}/*/*"
+}
+
+output "api_gateway_url" {
+  description = "URL for API Gateway"
+  value = aws_apigatewayv2_stage.lambda_stage.invoke_url
+}
+
+
