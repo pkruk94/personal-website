4f540402c55d7a101385f6ff07b9bf644e71652e
pkruk <pkruk94@gmail.com>
2026-01-21 16:51:26 +0100

    Added new workflow file, added new policies and roles for better separation

--- terraform/aws-backend/iamroles.tf
+++ terraform/aws-backend/iamroles.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "github-actions-infrastructure-deploy-role" {
   name               = "GitHubActionsInfrastructureDeployRole"
-  assume_role_policy = data.aws_iam_policy_document.oidc_assume_role_permissions.json
+  assume_role_policy = data.aws_iam_policy_document.oidc_apply_assume_role.json
 }
 
 resource "aws_iam_role_policy_attachment" "github_actions_infrastructure_policy_attachment" {
@@ -10,10 +10,20 @@ resource "aws_iam_role_policy_attachment" "github_actions_infrastructure_policy_
 
 resource "aws_iam_role" "github-actions-frontend-deploy-role" {
   name               = "GitHubActionsFrontendDeployRole"
-  assume_role_policy = data.aws_iam_policy_document.oidc_assume_role_permissions.json
+  assume_role_policy = data.aws_iam_policy_document.oidc_apply_assume_role.json
 }
 
 resource "aws_iam_role_policy_attachment" "github_actions_frontend_policy_attachment" {
   policy_arn = aws_iam_policy.deploy_frontend_policy.arn
   role       = aws_iam_role.github-actions-frontend-deploy-role.name
+}
+
+resource "aws_iam_role" "github-actions-infrastructure-plan-role" {
+  name = "GitHubActionsInfrastructurePlanRole"
+  assume_role_policy = data.aws_iam_policy_document.oidc_plan_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_plan_policy_attachment" {
+  policy_arn = aws_iam_policy.plan_infrastructure_policy.arn
+  role = aws_iam_role.github-actions-infrastructure-plan-role.name
 }
