620895aba3b120626f90e124d2ad1b5c61517fef
pkruk <pkruk94@gmail.com>
2026-01-31 13:56:32 +0100

    Reformat files

--- terraform/infra/apigw.tf
+++ terraform/infra/apigw.tf
@@ -4,30 +4,30 @@ resource "aws_apigatewayv2_api" "lambda_api" {
 }
 
 resource "aws_apigatewayv2_stage" "lambda_stage" {
-  api_id = aws_apigatewayv2_api.lambda_api.id
-  name   = "$default"
+  api_id      = aws_apigatewayv2_api.lambda_api.id
+  name        = "$default"
   auto_deploy = true
 }
 
 resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id           = aws_apigatewayv2_api.lambda_api.id
-  integration_type = "AWS_PROXY"
-  integration_uri = aws_lambda_function.visit_counter_lambda.invoke_arn
+  api_id                 = aws_apigatewayv2_api.lambda_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.visit_counter_lambda.invoke_arn
   payload_format_version = "2.0"
 }
 
 resource "aws_apigatewayv2_route" "lambda_route" {
-  api_id = aws_apigatewayv2_api.lambda_api.id
+  api_id    = aws_apigatewayv2_api.lambda_api.id
   route_key = "POST /"
-  target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
 resource "aws_lambda_permission" "api_gw_invoke" {
-  statement_id = "AllowExecutionFromAPIGateway"
+  statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.visit_counter_lambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn = "${aws_apigatewayv2_api.lambda_api.execution_arn}/*/*"
+  source_arn    = "${aws_apigatewayv2_api.lambda_api.execution_arn}/*/*"
 }
 
 
