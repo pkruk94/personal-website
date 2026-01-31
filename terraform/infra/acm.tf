408b7b8895bd051b199e9b9f72608bbf142f162e
pkruk <pkruk94@gmail.com>
2026-01-31 14:06:45 +0100

    Update acm, reformat outputs

--- terraform/infra/acm.tf
+++ terraform/infra/acm.tf
@@ -1,5 +1,6 @@
 resource "aws_acm_certificate" "ssl_certificate" {
-  domain_name = var.domain_name
+  count             = var.environment == "prod" ? 1 : 0
+  domain_name       = var.domain_name
   validation_method = "DNS"
 
   lifecycle {
@@ -11,14 +12,14 @@ resource "aws_acm_certificate" "ssl_certificate" {
 
 resource "cloudflare_dns_record" "ssl_certificate_validation_record" {
   for_each = {
-    for dvo in aws_acm_certificate.ssl_certificate.domain_validation_options : dvo.domain_name => {
+    for dvo in try(aws_acm_certificate.ssl_certificate[0].domain_validation_options, []) : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
     }
   }
 
-  zone_id = data.aws_ssm_parameter.cloudflare_zone_id.value
+  zone_id = data.aws_ssm_parameter.cloudflare_zone_id[0].value
   name    = each.value.name
   content = each.value.record
   type    = each.value.type
@@ -26,8 +27,11 @@ resource "cloudflare_dns_record" "ssl_certificate_validation_record" {
 }
 
 resource "aws_acm_certificate_validation" "ssl_certificate_validation" {
-  certificate_arn = aws_acm_certificate.ssl_certificate.arn
-  validation_record_fqdns = [for record in cloudflare_dns_record.ssl_certificate_validation_record : trimsuffix(record.name, ".")]
+  count                   = var.environment == "prod" ? 1 : 0
+  certificate_arn         = aws_acm_certificate.ssl_certificate[0].arn
+  validation_record_fqdns = [
+    for record in cloudflare_dns_record.ssl_certificate_validation_record :trimsuffix(record.name, ".")
+  ]
 
   timeouts {
     create = "5m"
